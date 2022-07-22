41e52c4b31e2b8dad91ad66feeb9bec41804477d
Jonathan <jonathancqaa@gmail.com>
2022-07-22 15:33:39 -0300

    Projeto lista 9 Locadora Dotnet Grupo 4

---
+++ Locadora/Program.cs
@@ -0,0 +1,142 @@
+﻿using Locadora.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Locadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool on = true;
+            int resp;
+
+            List<Filmes> filmes = new List<Filmes>();
+            filmes.Add(new Filmes("Piratas do Caribe", 14, 120, "Disney", "Ingles - Português", "3D", Categoria.ACAO));
+            filmes.Add(new Filmes("Para Todos os Garotos que já Amei", 15, 120, "NETFLIX", "Ingles - Português", "2D", Categoria.ROMANCE));
+            filmes.Add(new Filmes("Interestelar", 14, 120, "Lengedary", "Ingles - Português", "3D", Categoria.FICCAO));
+            filmes.Add(new Filmes("Sim Senhor", 14, 120, "Warner Bros. Pictures", "Ingles - Português", "2D", Categoria.COMEDIA));
+            filmes.Add(new Filmes("O Homem Invisivel ", 16, 120, "Universal Pictures", "Ingles - Português", "3D", Categoria.SUSPENSE));
+
+            do {
+                Console.WriteLine("===== LOCADORA DOTNET GRUPO 4 =====");
+                Console.WriteLine("1 - COMEDIA\n" +
+                                  "2 - ROMANCE\n" +
+                                  "3 - FICCAO\n" +
+                                  "4 - ACAO\n" +
+                                  "5 - SUSPENSE\n" +
+                                  "0 - SAIR DA LOCADORA\n");
+                Console.Write("Digite a Categoria que você deseja: ");
+                int menu = int.Parse(Console.ReadLine());               
+
+
+                switch (menu)
+                {
+                    case 1:
+
+                        int alugado = 0;
+                        
+                           
+                            Console.WriteLine("Filme Disponivel:");
+                            Console.WriteLine(filmes[3]);
+                            Console.WriteLine("Deseja alugar esse filme? \n" +
+                                              "1 - Sim | 2 - Não");
+                            resp = int.Parse(Console.ReadLine());
+                            if (resp == 1)
+                            {
+                            if (alugado == 1) 
+                            {
+                                Console.Write("Filme Alugado \n");
+                                //filmes.Insert(3,);
+                                alugado++;
+                                continue;
+                            }
+                            }
+                            else if (resp == 2)
+                            {
+
+                            }
+                           
+                        
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Filme Disponivel:");
+                        Console.WriteLine(filmes[1]);
+                        Console.WriteLine("Deseja alugar esse filme? \n" +
+                                          "1 - Sim | 2 - Não");
+                        resp = int.Parse(Console.ReadLine());
+                        if (resp == 1)
+                        {
+                            Console.Write("Filme Alugado \n");
+                            on = false;
+                        }
+                        else if (resp == 2)
+                        {
+
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Filme Disponivel:");
+                        Console.WriteLine(filmes[2]);
+                        Console.WriteLine("Deseja alugar esse filme? \n" +
+                                          "1 - Sim | 2 - Não");
+                        resp = int.Parse(Console.ReadLine());
+                        if (resp == 1)
+                        {
+                            Console.Write("Filme Alugado \n");
+                            on = false;
+                        }
+                        else if (resp == 2)
+                        {
+
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Filme Disponivel:");
+                        Console.WriteLine(filmes[0]);
+                        Console.WriteLine("Deseja alugar esse filme? \n" +
+                                          "1 - Sim | 2 - Não");
+                        resp = int.Parse(Console.ReadLine());
+                        if (resp == 1)
+                        {
+                            Console.Write("Filme Alugado \n");
+                            on = false;
+                        }
+                        else if (resp == 2)
+                        {
+
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Filme Disponivel:");
+                        Console.WriteLine(filmes[4]);
+                        Console.WriteLine("Deseja alugar esse filme? \n" +
+                                          "1 - Sim | 2 - Não");
+                        resp = int.Parse(Console.ReadLine());
+                        if (resp == 1)
+                        {
+                            Console.Write("Filme Alugado \n");
+                            on = false;
+                        }
+                        else if (resp == 2)
+                        {
+
+                        }
+                        break;
+
+                    case 0:
+                        Console.WriteLine("");
+                        on = false;
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Digite a opção correspondente ao menu!");
+                        break;
+                }
+
+            } while (on == true);
+        }
+    }
+}
---
+++ Locadora/src/Filmes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.src
+{
+    public class Filmes
+    {
+        public string Titulo { get; set; }
+        public int Classificacao { get; set; }
+        public int Duracao { get; set; }
+        public string Estudio { get; set; }
+        public string Idioma { get; set; }
+        public string TipoReproducao { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public Filmes(string titulo,
+            int classificacao, int duracao, string estudio, string idioma, string tipoReproducao, Categoria categoria)
+        {
+            Titulo = titulo;        
+            Classificacao = classificacao;
+            Duracao = duracao;
+            Estudio = estudio;
+            Idioma = idioma;
+            TipoReproducao = tipoReproducao;
+            Categoria = categoria;
+        }
+
+        public override string ToString()
+        {
+            return $"\n Título do filme: {Titulo}" +
+                $"\n Categoria: {Categoria}" +
+                $"\n Classificação Indicativa: {Classificacao}" +
+                $"\n Duração: {Duracao}" +
+                $"\n Estúdio: {Estudio}" +
+                $"\n Idioma: {Idioma}" +
+                $"\n Tipo de Reprodução: {TipoReproducao}";
+        }
+        
+    }
+
+    public enum Categoria
+    {
+        COMEDIA, ROMANCE, FICCAO, ACAO, SUSPENSE
+    }
+}
